0c6f2492fb07de0721a686f2fb009901f586d112
unknown <uyerandisr@gmail.com>
2024-04-13 17:29:33 -0400

    implementing form blog component

---
+++ src/app/blog/blog-form/blog-form.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-form',
+  standalone: true,
+  imports: 
+  [
+    CommonModule,
+  ],
+  templateUrl: './blog-form.component.html',
+  styleUrl: './blog-form.component.css'
+})
+export class BlogFormComponent 
+{
+
+}
